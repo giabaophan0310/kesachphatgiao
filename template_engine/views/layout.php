bf10c171265dd3b632c576532e493648cb160a1e
btoann <nguyenbaotoan2001@gmail.com>
2020-12-29 12:55:23 +0700

    .

--- template_engine/views/layout.php
+++ template_engine/views/layout.php
@@ -3,13 +3,7 @@
 <head>
     <title>Demo</title>
 
-    <?= (! @(file_get_contents('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css')))
-            ? '<link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">'
-            : '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    rel="stylesheet" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">';
-    ?>
+    <?php $this->renderSection('head'); ?>
 
 </head>
 
--- template_engine/views/profile.php
+++ template_engine/views/profile.php
@@ -1,26 +1,37 @@
 <?php $this->layout('layout') ?>
 
+<!--
+    HEAD
+-->
 <?php
     $this->section('head');
     $headBool = new boolean();
 ?>
 <?= $headBool->issetURL(
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
-                '<link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">',
                 '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">'
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">',
+                '<link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">'
             )
 ?>
-<script src="<?= $headBool->issetURL('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', null, '../public/js/jquery_3.5.1.js') ?>">
+<script src="<?= $headBool->issetURL('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', null, '../public/js/jquery_3.5.1.js') ?>"></script>
 <?php
     unset($headBool);
     $this->end();
 ?>
 
+
+<!--
+    CONTENT
+-->
 <?php $this->section('content'); ?>
     <h1> User Profile </h1> <p> Hello, <?php echo $name; ?> </p>
 <?php $this->end(); ?>
 
+
+<!--
+    SIDEBAR
+-->
 <?php $this->section('sidebar'); ?>
     <h2> Sidebar </h2>
 <?php $this->end(); ?>
