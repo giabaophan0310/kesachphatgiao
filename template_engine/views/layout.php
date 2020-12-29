e33398fb3c781787a15ddbe33674020308ce2fd6
btoann <nguyenbaotoan2001@gmail.com>
2020-12-29 12:00:21 +0700

    Demo

--- template_engine/views/layout.php
+++ template_engine/views/layout.php
@@ -8,10 +8,23 @@
 
     <?php $this->renderSection('sidebar'); ?>
 
-    <br />
+    <br>
 
     <?php $this->renderSection('content'); ?>
 
+    <br>
+    <?php
+
+$url = 'https://www.google.com/';
+$not_url = 'stp://google.com';
+
+if (@file_get_contents($url)): echo "Found '$url'!";
+else: echo "Can't find '$url'.";
+endif;
+if (@file_get_contents($not_url)): echo "Found '$not_url!";
+else: echo "Can't find '$not_url'.";
+endif;
+?>
 </body>
 
 </html>
