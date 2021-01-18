8b504ddfa6189b2c42e5f57f175b0bfa82bdc371
Bao <giabaonguyen1900@gmail.com>
2021-01-18 22:41:03 +0700

    leader-cọc-v
    không hiểu sao

---
+++ client/index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/client/index.css">
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+    <script src="../public/bootstrap/js/bootstrap.min.js"></script>
+    <title>Kệ sách Phật Giáo</title>
+    
+</head>
+<body>
+    <header>
+        <nav class="top-menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="menu-logo">
+                                <a href="#"><img src="../../../images/accounts/logo_demo.png" alt=""></a>
+                            </div>
+                            <div class="menu-search">
+                                <form action="" method="post">
+                                    <input type="text" name="" id="" placeholder="Tìm kiếm">
+                                    <input type="submit" value="Tìm kiếm">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-4 p-0 d-flex justify-content-end">
+                        <div class="menu-login">
+                            <input type="submit" value="Đăng nhập">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="top-head-menu">
+            <div class="container over-show">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="top-head-menu-wrapper">
+                            <ul class="root-menu">
+                                <li>
+                                    <a href="#">Giới thiệu</a>
+                                    <div class="top-head-sub-menu">
+                                        <div class="top-head-sub-content th-menu">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Mục đích</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Người sáng lập</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Công ty Hoa Lan Thanh Quang</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Công ty sâm Ngọc Linh</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"> Viện Nghiên Cứu và Ứng Dụng Buddha YoGa</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="">Hoạt động</a>
+                                    <div class="top-head-sub-menu">
+                                        <div class="top-head-sub-content th-menu">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Thông báo</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Tin Tức</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Hình Ảnh</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="">Triết lý</a>
+                                    <div class="top-head-sub-menu">
+                                        <div class="top-head-sub-content th-menu">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Luận Giảng</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Khai Thị luận</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Ứng Dụng</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Sách nói</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="">Thư viện bài giảng</a>
+                                    <div class="top-head-sub-menu">
+                                        <div class="top-head-sub-content th-menu">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Video</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Mp3</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <a href="">Câu chuyện</a>
+                                    <div class="top-head-sub-menu">
+                                        <div class="top-head-sub-content th-menu">
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Thực hành thực chứng kết quả nhập thế</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Bài viết</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Cảm nhận</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- <div class="top-head-menu">
+            <div class="container over-show">
+                <div class="top-head-menu-wrapper">
+                    
+                </div>
+                
+            </div>
+        </div> -->
+    </header>
+    
+    <div style="display: block;">
+        <main>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="swiper-container swiper-container-horizontal">
+                            <div class="swiper-wrapper">
+                                
+                            </div>
+                            <div class="swiper-button-next" id="slider-home-next"></div>
+                            <div class="swiper-button-prev" id="slider-home-prev"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container mt-4 over-show">
+                    <div class="row">
+                        <div class="rounded sidebar-col">
+                            <div class="row box-shadow bg-white sidebar-box pt-2 rounded">
+                                <div class="left-sidebar col">
+                                    <div class="left-sidebar-header">
+                                        <div class="row">
+                                            <div class="col">
+                                                <p>Danh mục</p>
+                                            </div>
+                                            <div class="col-2 d-flex justify-content-center">
+                                                <a href="#"><div class="left-sidebar-toggle-btn"><img src="../../../images/accounts/menu-icon.png" alt=""></div></a>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col p-0 d-flex flex-row-reverse">
+                            <div class="book-audio">
+                                <div class="book-audio-header">
+                                    <div class="col p-0 d-flex justify-content-start">
+                                        <h2>Title</h2>
+                                    </div>
+
+                                    <div class="col p-0 d-flex justify-content-end font-tabs">
+                                        <ul class="nav nav-tabs">
+                                            <li class="nav-item">
+                                              <a class="nav-link active" href="#">Ngày</a>
+                                            </li>
+                                            <li class="nav-item">
+                                              <a class="nav-link" href="#">Tuần</a>
+                                            </li>
+                                            <li class="nav-item">
+                                              <a class="nav-link" href="#">Tháng</a>
+                                            </li>
+                                            <li class="nav-item">
+                                              <a class="nav-link" href="#">Tổng</a>
+                                            </li>
+                                          </ul>
+                                    </div>
+                                </div>
+                                    <div class="book-audio-content">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="book-row">
+                                                    <div class="title-book">Title</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-6 pl-0">
+                                                <div class="book-row">
+                                                    <div class="title-book">Title</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container mt-4">
+                    <div class="row">
+                        <div class="news-box bg-news">
+                            <h2>Các phát hành mới của trang web</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container mt-2 news-week">
+                    <div class="row">
+                        <div class="news-week-box col">
+                            <div class="news-week-header ">
+                                <div class="row">
+                                    <div class="col-6 p-0 d-flex justify-content-start">
+                                        <h2><a href="#">Mới trong tuần</a></h2>
+                                    </div>
+                                    <div class="col-6 p-0 d-flex justify-content-end">
+                                        <a href="#">
+                                            xem tất cả
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container mt-4 over-show">
+                    <div class="row">
+                        
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="video-box">
+                            <div class="container pt-3">
+                                <div class="video-box-header">
+                                    <div class="row full-h">
+                                        VIDEO
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container-fluid mt-4">
+                    <div class="row">
+                        <div class="customer-quote-box">
+                            <div class="container">
+                                Các câu nói quote
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="footer-box">
+                        <div class="col d-flex justify-content-center">&copy; trietlybudhha</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+</body>
+</html>
